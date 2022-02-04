c5d6156705cf9532e580f6bc0989cacb37c294da
Carl Peto <carl.peto@me.com>
2022-02-04 13:12:32 +0000

    This works in conjunction with github.com/swiftforarduino/llvm-project branch avr-swift-progmem-addrspace-1 (currently at commit 4419b03ade2a).
    It updates Swift, at least partially, to be aware of address spaces for functions.
    
    for closures, allow round trip via opaque ptr in pgmspace
    
    - Fix for mapping program space functions into protocol witness tables and value witness tables
    - Fixed in SIL function lowering, addPointerParameter should only create pointers in address space 1 when the parameter is a function (e.g. working with closures).
    
    fix coroutine/yield emission
    
    more program address space fixes
    
    addrsp
    
    minor rename
    
    - fix pointer wrangling between address spaces (or not) in coroutine emission
    - add a variant build script for mac mini server
    
    - fix pointer parameter wrangling between namespaces in call site emission
    
    try making something that will work with ARM and AVR, generalised

--- lib/IRGen/GenProto.cpp
+++ lib/IRGen/GenProto.cpp
@@ -1391,7 +1391,7 @@ public:
               IGM.getDeletedMethodErrorFn(), IGM.FunctionPtrTy);
         }
       }
-      witness = llvm::ConstantExpr::getBitCast(witness, IGM.Int8PtrTy);
+      witness = llvm::ConstantExpr::getPointerBitCastOrAddrSpaceCast(witness, IGM.Int8PtrTy);
 
       PointerAuthSchema schema =
           isAsyncRequirement
