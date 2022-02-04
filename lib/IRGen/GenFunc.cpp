4f6da8ad8f9b8cf59503507a1082c5028ac29a5b
Carl Peto <carl.peto@me.com>
2022-02-04 13:12:32 +0000

    This works in conjunction with github.com/swiftforarduino/llvm-project branch avr-swift-progmem-addrspace-1 (currently at commit 4419b03ade2a).
    It updates Swift, at least partially, to be aware of address spaces for functions.
    
    for closures, allow round trip via opaque ptr in pgmspace
    
    - Fix for mapping program space functions into protocol witness tables and value witness tables
    - Fixed in SIL function lowering, addPointerParameter should only create pointers in address space 1 when the parameter is a function (e.g. working with closures).
    
    fix coroutine/yield emission
    
    more program address space fixes
    
    addrsp
    
    minor rename
    
    - fix pointer wrangling between address spaces (or not) in coroutine emission
    - add a variant build script for mac mini server
    
    - fix pointer parameter wrangling between namespaces in call site emission
    
    try making something that will work with ARM and AVR, generalised

--- lib/IRGen/GenFunc.cpp
+++ lib/IRGen/GenFunc.cpp
@@ -1666,7 +1666,7 @@ static llvm::Value *emitPartialApplicationForwarder(IRGenModule &IGM,
   }
 
   // Derive the callee function pointer.
-  auto fnTy = origSig.getType()->getPointerTo();
+  auto fnTy = origSig.getType()->getPointerTo(IGM.DataLayout.getProgramAddressSpace());
   FunctionPointer fnPtr = [&]() -> FunctionPointer {
     // If we found a function pointer statically, great.
     if (staticFnPtr) {
@@ -2124,7 +2124,7 @@ Optional<StackAddress> irgen::emitFunctionPartialApplication(
       IGF.IGM, staticFn, fnContext != nullptr, origSig, origType, substType,
       outType, subs, &layout, argConventions);
   forwarder = emitPointerAuthSign(IGF, forwarder, outAuthInfo);
-  forwarder = IGF.Builder.CreateBitCast(forwarder, IGF.IGM.Int8PtrTy);
+  forwarder = IGF.Builder.CreateBitCast(forwarder, IGF.IGM.Int8ProgramSpacePtrTy);
   out.add(forwarder);
   out.add(data);
   return stackAddr;
