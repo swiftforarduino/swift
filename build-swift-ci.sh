1cbac09911cdcc96aacc0bb1cb25354753df0617
Carl Peto <carl.peto@me.com>
2022-07-23 09:09:42 +0100

    ci scripts

---
+++ build-swift-ci.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -ex
+
+if [[ "$1" == "debug" ]]
+then
+  BUILD_SUBDIR=Ninja-ReleaseAssert+swift-DebugAssert
+else
+  BUILD_SUBDIR=Ninja-ReleaseAssert
+fi
+
+PLATFORM=macosx
+ARCH=x86_64
+
+cd build
+cd ${BUILD_SUBDIR}
+pushd cmark-${PLATFORM}-${ARCH}
+ninja
+popd
+pushd llvm-${PLATFORM}-${ARCH}
+ninja
+popd
+pushd swift-${PLATFORM}-${ARCH}
+ninja swift
+ninja swift-ide-test
+popd
+
+echo "** COMPLETED SWIFT COMPILER BUILD **"
---
+++ checkout-swift-ci.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/arch -arch x86_64 /bin/bash -x
+
+PATH=$PATH:/Applications/CMake.app/Contents/bin
+
+git clone --depth=1 -b release/5.3 https://github.com/apple/swift-cmark.git cmark
+git clone --depth=1 -b avr-swift-progmem-addrspace-1 git@github.com:swiftforarduino/llvm-project.git llvm-project
+git clone --depth=1 -b main git@github.com:swiftforarduino/AVR2.git AVR2
+
+if [[ "${CI_SERVER}" == "" ]]
+then
+  git clone --depth=1 -b avr-support-8 git@github.com:swiftforarduino/swift.git swift
+fi
+
+ln -s ../AVR2/uSwift/Runtime swift/uSwiftRuntime
+
+ln -s llvm-project/clang clang
+ln -s llvm-project/llvm llvm
+
+cd swift
+
+if [[ "$1" == "debug" ]]
+then
+  DEBUG_SWIFT_OPT=--debug-swift
+else
+  DEBUG_SWIFT_OPT=
+fi
+
+utils/build-script -R -S --clean ${DEBUG_SWIFT_OPT} --extra-cmake-options="-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=AVR"  --extra-cmake-options="-DLLVM_ENABLE_PROJECTS='clang'"
+
+echo "** COMPLETED SWIFT CHECKOUT SOURCES AND CONFIGURATION **"
