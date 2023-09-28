e781afe8f5c4d0332e08475bdbf1b9af8a36f852
Carl Peto <carl.peto@me.com>
2023-09-28 08:25:15 +0100

    add my scripting/CI files to see if we can get a clean build on the CI

---
+++ build-swift-ci.sh
@@ -0,0 +1,31 @@
+#! /bin/bash -ex
+
+if [[ "$1" == "fulldebug" ]]
+then
+  BUILD_SUBDIR=Ninja-DebugAssert
+else
+  if [[ "$1" == "debug" ]]
+  then
+    BUILD_SUBDIR=Ninja-ReleaseAssert+swift-DebugAssert
+  else
+    BUILD_SUBDIR=Ninja-ReleaseAssert
+  fi
+fi
+
+PLATFORM=macosx
+ARCH=x86_64
+
+cd build
+cd ${BUILD_SUBDIR}
+pushd cmark-${PLATFORM}-${ARCH}
+ninja
+popd
+pushd llvm-${PLATFORM}-${ARCH}
+ninja
+popd
+pushd swift-${PLATFORM}-${ARCH}
+ninja swift-frontend
+ninja swift-ide-test
+popd
+
+echo "** COMPLETED SWIFT COMPILER BUILD **"
