9cb8e3ff5f7bdcf320b4dc4f24fb7ca64501078b
Carl Peto <carl.peto@me.com>
2023-11-01 10:24:24 +0000

    bump min version

--- checkout-swift-ci.sh
+++ checkout-swift-ci.sh
@@ -92,7 +92,7 @@ fi
 
 
 utils/build-script -R -S --clean ${DEBUG_SWIFT_OPT} --extra-cmake-options="-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=AVR;ARM"  --extra-cmake-options="-DLLVM_ENABLE_PROJECTS='clang'" \
---skip-build-benchmarks --skip-ios --skip-watchos --skip-tvos --darwin-deployment-version-osx 10.15 \
+--skip-build-benchmarks --skip-ios --skip-watchos --skip-tvos --darwin-deployment-version-osx 11.0 \
 --skip-early-swift-driver=true --skip-early-swiftsyntax=true \
 --swift-driver=false --swift-disable-dead-stripping #--bootstrapping=
 # --swift-darwin-supported-archs "$(uname -m)"
