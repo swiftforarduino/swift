0d430fa8dc0ab63e52a0ab25c75c20ed53df5cef
Carl Peto <CPeto@becrypt.com>
2025-07-26 17:55:30 +0100

    CI added external HD tidy up the staging section and make release builds default (no point having the overheads of DWARF info, assertions are good enough if we don't want proper debug) arm64 path

---
+++ build-swift-ci.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -ex
+
+PLATFORM=macosx
+ARCH=x86_64
+
+cd swift
+
+utils/build-script --skip-build-benchmarks \
+  --extra-cmake-options="-DSWIFT_SHOULD_BUILD_EMBEDDED_STDLIB_CROSS_COMPILING=YES" \
+  --swift-darwin-supported-archs "x86_64;arm64" \
+  --${RELEASE_TYPE_FLAGS} --swift-disable-dead-stripping \
+  --bootstrapping=hosttools --sccache
+
+echo "** COMPLETED SWIFT COMPILER BUILD **"
---
+++ checkout-swift-ci.sh
@@ -0,0 +1,75 @@
+#! /usr/bin/arch -arch x86_64 /bin/bash -ex
+
+PATH=$PATH:/Applications/CMake.app/Contents/bin
+
+# iota uSwift stdlib/runtime repository
+USWIFT_RUNTIME_SUB_PATH=uSwift/Runtime
+USWIFT_RUNTIME_URL=git@github.com:swiftforarduino/AVR2.git
+USWIFT_RUNTIME_BRANCH=main
+USWIFT_RUNTIME_LOCAL_DIR=AVR2
+
+# setup uSwift - not sure this is used any more on this branch?
+rm -rf $USWIFT_RUNTIME_LOCAL_DIR
+git clone --depth=1 -b $USWIFT_RUNTIME_BRANCH $USWIFT_RUNTIME_URL $USWIFT_RUNTIME_LOCAL_DIR
+rm -f swift/uSwiftRuntime
+ln -s $USWIFT_RUNTIME_LOCAL_DIR/$USWIFT_RUNTIME_SUB_PATH swift/uSwiftRuntime
+
+cd swift
+
+# utils/update-checkout --clone-with-ssh --clone-depth 1 --skip-repository swift
+
+# Created by ChatGPT so it might be guff...
+utils/update-checkout \
+  --clone-with-ssh \
+  --skip-history \
+  --skip-repository swift \
+  --skip-repository swift-log \
+  --skip-repository swift-async-algorithms \
+  --skip-repository swift-corelibs-xctest \
+  --skip-repository swift-stress-tester \
+  --skip-repository swift-integration-tests \
+  --skip-repository swift-corelibs-foundation \
+  --skip-repository swift-foundation-icu \
+  --skip-repository swift-corelibs-libdispatch \
+  --skip-repository swift-foundation \
+  --skip-repository swift-xcode-playground-support \
+  --skip-repository swift-llvm-bindings \
+  --skip-repository swift-sdk-generator \
+  --skip-repository swift-nio \
+  --skip-repository zlib \
+  --skip-repository libxml2 \
+  --skip-repository curl \
+  --skip-repository cmake \
+  --skip-repository wasi-libc \
+  --skip-repository wasmkit
+
+# seems to be needed by swift-driver
+# swift-argument-parser
+# swift-tools-support-core
+
+# needed by the swift regex...
+# swift-experimental-string-processing
+
+# required to build toolchains...
+# swift-installer-scripts
+
+# needed by swiftpm
+# swift-crypto
+# swift-certificates
+# swift-collections
+# swift-toolchain-sqlite
+# swift-system
+# llbuild
+# swift-build
+# swift-asn (transitive)
+
+# needed by sourcekit-lsp
+# indexstore-db
+# swift-lmdb (transitive)
+
+echo "** COMPLETED SWIFT CHECKOUT SOURCES **"
+
+if [[ $2 == andbuild ]]
+then
+  ./build-swift-ci.sh $1
+fi
