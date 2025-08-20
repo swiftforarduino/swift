4bdf08d5d9fca9b34bc1b460062603897f3cf0bf
Carl Peto <carl.peto@me.com>
2025-08-20 18:53:08 +0100

    try to suppress shared libs and build static, then have statically li… (#13)

--- build-swift-ci.sh
+++ build-swift-ci.sh
@@ -6,7 +6,7 @@ ARCH=x86_64
 cd swift
 
 utils/build-script --skip-build-benchmarks \
-  --extra-cmake-options="-DSWIFT_SHOULD_BUILD_EMBEDDED_STDLIB_CROSS_COMPILING=YES" \
+  --extra-cmake-options="-DSWIFT_SHOULD_BUILD_EMBEDDED_STDLIB_CROSS_COMPILING=YES -DBUILD_SHARED_LIBS=NO" \
   --swift-darwin-supported-archs "x86_64;arm64" \
   --${RELEASE_TYPE_FLAGS} --swift-disable-dead-stripping \
   --bootstrapping=hosttools --sccache
