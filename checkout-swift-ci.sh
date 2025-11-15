cb22fd690c737dfc235e9fe4e505af3f9ddaee06
Carl Peto <carl.peto@me.com>
2025-11-15 12:45:14 +0000

    attempt some simplifications for handing over the compier build to Axel and Paul

--- checkout-swift-ci.sh
+++ checkout-swift-ci.sh
@@ -1,28 +1,28 @@
 #! /usr/bin/arch -arch x86_64 /bin/bash -ex
 
-PATH=$PATH:/Applications/CMake.app/Contents/bin
+# PATH=$PATH:/Applications/CMake.app/Contents/bin
 
-# iota uSwift stdlib/runtime repository
-USWIFT_RUNTIME_SUB_PATH=uSwift/Runtime
-USWIFT_RUNTIME_URL=git@github.com:swiftforarduino/AVR2.git
-USWIFT_RUNTIME_BRANCH=main
-USWIFT_RUNTIME_LOCAL_DIR=AVR2
+# # iota uSwift stdlib/runtime repository
+# USWIFT_RUNTIME_SUB_PATH=uSwift/Runtime
+# USWIFT_RUNTIME_URL=git@github.com:swiftforarduino/AVR2.git
+# USWIFT_RUNTIME_BRANCH=main
+# USWIFT_RUNTIME_LOCAL_DIR=AVR2
 
-# setup uSwift - not sure this is used any more on this branch?
-rm -rf $USWIFT_RUNTIME_LOCAL_DIR
-git clone --depth=1 -b $USWIFT_RUNTIME_BRANCH $USWIFT_RUNTIME_URL $USWIFT_RUNTIME_LOCAL_DIR
-rm -f swift/uSwiftRuntime
-ln -s $USWIFT_RUNTIME_LOCAL_DIR/$USWIFT_RUNTIME_SUB_PATH swift/uSwiftRuntime
+# # setup uSwift - not sure this is used any more on this branch?
+# rm -rf $USWIFT_RUNTIME_LOCAL_DIR
+# git clone --depth=1 -b $USWIFT_RUNTIME_BRANCH $USWIFT_RUNTIME_URL $USWIFT_RUNTIME_LOCAL_DIR
+# rm -f swift/uSwiftRuntime
+# ln -s $USWIFT_RUNTIME_LOCAL_DIR/$USWIFT_RUNTIME_SUB_PATH swift/uSwiftRuntime
 
 cd swift
 
-if [[ -d ../swift-driver ]]
-then
-  pushd ../swift-driver
-  git restore .
-  git clean -f .
-  popd
-fi
+# if [[ -d ../swift-driver ]]
+# then
+#   pushd ../swift-driver
+#   git restore .
+#   git clean -f .
+#   popd
+# fi
 
 # utils/update-checkout --clone-with-ssh --clone-depth 1 --skip-repository swift
 
@@ -44,12 +44,18 @@ utils/update-checkout \
   --skip-repository swift-llvm-bindings \
   --skip-repository swift-sdk-generator \
   --skip-repository swift-nio \
-  --skip-repository zlib \
-  --skip-repository libxml2 \
-  --skip-repository curl \
-  --skip-repository cmake \
   --skip-repository wasi-libc \
   --skip-repository wasmkit
+
+# we are now going to attempt to build zlib, libxml2, curl and cmake too
+# they shouldn't take too long, and it should remove the dependency on
+# cmake being installed on the machine
+  # --skip-repository zlib \
+  # --skip-repository libxml2 \
+  # --skip-repository curl \
+  # --skip-repository cmake \
+
+
 
 # seems to be needed by swift-driver
 # swift-argument-parser
@@ -85,7 +91,7 @@ popd
 
 echo "** COMPLETED SWIFT CHECKOUT SOURCES **"
 
-if [[ $2 == andbuild ]]
-then
-  ./build-swift-ci.sh $1
-fi
+# if [[ $2 == andbuild ]]
+# then
+#   ./build-swift-ci.sh $1
+# fi
