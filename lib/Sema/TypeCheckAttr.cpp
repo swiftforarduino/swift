fa3a3dc84797d14cb0a0ba5a17798c6389fbf3bc
Carl Peto <carl.peto@me.com>
2025-12-29 09:28:54 +0000

    fix issued caused by incorrect rebase

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -206,7 +206,6 @@ public:
   IGNORED_ATTR(AllowFeatureSuppression)
   IGNORED_ATTR(PreInverseGenerics)
   IGNORED_ATTR(Safe)
-  IGNORED_ATTR(Warn)
   IGNORED_ATTR(InterruptHandler)
 #undef IGNORED_ATTR
 
