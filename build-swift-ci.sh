6302b0f882d68312a1e847a3427e8edef611e764
Carl Peto <carl.peto@me.com>
2026-01-01 02:15:13 +0000

    attempt to remove zstd dependency

--- build-swift-ci.sh
+++ build-swift-ci.sh
@@ -3,7 +3,7 @@
 cd swift
 
 utils/build-script --skip-build-benchmarks  --skip-ios --skip-watchos --skip-xros \
-  --extra-cmake-options="-DBUILD_SHARED_LIBS=NO,-DSWIFT_SHOULD_BUILD_EMBEDDED_STDLIB_CROSS_COMPILING=YES" \
+  --extra-cmake-options="-DBUILD_SHARED_LIBS=NO,-DSWIFT_SHOULD_BUILD_EMBEDDED_STDLIB_CROSS_COMPILING=YES,-DLLVM_ENABLE_ZSTD=NO" \
   --swift-darwin-supported-archs "arm64" \
   --${RELEASE_TYPE_FLAGS} --swift-disable-dead-stripping \
   --bootstrapping=hosttools --sccache --static-zlib TRUE --static-libxml2 TRUE
