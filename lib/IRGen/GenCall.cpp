0b2f4561ff702a21f2da223e37826d6eaf19feaf
Carl Peto <carl.peto@me.com>
2026-08-30 00:49:19 +0100

    Bug Brent found where most lazy loaded globals are causing the compiler to crash (address space issues on accessor function).

--- lib/IRGen/GenCall.cpp
+++ lib/IRGen/GenCall.cpp
@@ -6422,7 +6422,11 @@ Signature irgen::emitCastOfFunctionPointer(IRGenFunction &IGF,
                             : IGF.IGM.getSignature(fnType);
 
   // Emit the cast.
-  fnPtr = IGF.Builder.CreateBitCast(fnPtr, IGF.IGM.FunctionPtrTy);
+  if (fnPtr->getType()->getPointerAddressSpace()!=0) {
+    fnPtr = IGF.Builder.CreateBitCast(fnPtr, IGF.IGM.FunctionPtrTy);
+  } else {
+    fnPtr = IGF.Builder.CreateBitCast(fnPtr, IGF.IGM.PtrTy);
+  }
 
   // Return the information.
   return sig;
